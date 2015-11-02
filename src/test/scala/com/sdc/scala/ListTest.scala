f61209f7618806170d36c22e5ae6366710423b44
tmucha82@gmail.com <czekolada%5>
2015-11-02 16:27:02 +0100

    Chapter 16

--- src/test/scala/com/sdc/scala/ListTest.scala
+++ src/test/scala/com/sdc/scala/ListTest.scala
@@ -2,6 +2,8 @@ package com.sdc.scala
 
 import org.scalatest.FunSuite
 
+import scala.util.Random
+
 class ListTest extends FunSuite {
   test("initialization of list") {
     val fruit = "apples" :: "oranges" :: "pears" :: Nil //the same as: List("apples", "oranges", "pears")
@@ -54,5 +56,88 @@ class ListTest extends FunSuite {
 
     assert(List(1, 2, 3, 4) === append(List(1, 2), List(3, 4)))
     assert(List(1, 2, 3, 4, 5, 6) === append(List(1, 2), append(List(3, 4), List(5, 6))))
+  }
+
+  test("init and last element of list") {
+    val letters = List('a', 'b', 'c', 'd', 'e')
+
+    //this is more expensive than head and tail, like list.isEmpty and list.length == 0
+    assert('e' === letters.last)
+    assert(List('a', 'b', 'c', 'd') === letters.init)
+
+    //like head and tail
+    assert('a' === letters.head)
+    assert(List('b', 'c', 'd', 'e') === letters.tail)
+
+    intercept[NoSuchElementException] {
+      List().last
+    }
+    intercept[UnsupportedOperationException] {
+      List().init
+    }
+    intercept[NoSuchElementException] {
+      List().head
+    }
+    intercept[UnsupportedOperationException] {
+      List().tail
+    }
+  }
+
+  test("reverse of list") {
+    val letters = List('a', 'b', 'c', 'd', 'e')
+    assert(letters.reverse.reverse == letters)
+    assert(letters.reverse.tail == letters.init.reverse) //d c b a
+    assert(letters.reverse.init == letters.tail.reverse) //e d c b
+    assert(letters.reverse.head === letters.last) //e
+    assert(letters.reverse.last === letters.head) //a
+  }
+
+  test("own revers of list") {
+    def reverse[T](list: List[T]): List[T] = list match {
+      case List() => list
+      case element :: subList => reverse(subList) ::: List(element)
+    }
+    val letters = List('a', 'b', 'c', 'd', 'e')
+    println(reverse(letters))
+  }
+
+  test("prefixes and suffixes: drop, take and splitAt") {
+    val letters = List('a', 'b', 'c', 'd', 'e')
+
+    val n = new Random().nextInt(letters.length)
+    assert(letters === letters.take(n) ::: letters.drop(n))
+
+    //take - first n
+    assert(List(letters.head) === letters.take(1))
+    assert(letters.init === letters.take(letters.length - 1))
+
+    //drop - first n
+    assert(letters.tail === letters.drop(1))
+    assert(List(letters.last) === letters.drop(letters.length - 1))
+
+    //splitAt n - take n ::: drop n
+    assert((List(letters.head), letters.tail) === letters.splitAt(1))
+    assert((letters.init, List(letters.last)) === letters.splitAt(letters.length - 1))
+  }
+
+  test("element selection: apply and indices") {
+    val letters = List('a', 'b', 'c', 'd', 'e')
+    val n = new Random().nextInt(letters.length)
+
+    //apply - like get
+    assert('b' === letters.apply(1))
+    assert(letters.last === letters.apply(letters.length - 1))
+    assert(letters.drop(n).head === letters.apply(n))
+    assert(List(0, 1, 2, 3, 4) === letters.indices) // a b c d e
+  }
+
+  test("zipping lists: zip") {
+    val letters = List('a', 'b', 'c', 'd', 'e') // a b c d e
+    val indexes = letters.indices //0 1 2 3 4
+
+    assert(List(('a', 0), ('b', 1), ('c', 2), ('d', 3), ('e', 4)) === letters.zip(indexes))
+    assert(List((0, 'a'), (1, 'b'), (2, 'c'), (3, 'd'), (4, 'e')) === indexes.zip(letters))
+    assert(List(('a', 'a'), ('b', 'b'), ('c', 'c'), ('d', 'd'), ('e', 'e')) === letters.zip(letters))
+    assert(List((0, 0), (1, 1), (2, 2), (3, 3), (4, 4)) === indexes.zip(indexes))
   }
 }
