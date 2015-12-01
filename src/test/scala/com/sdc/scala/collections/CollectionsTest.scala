cec1caa7ed8d4128b2f2b7c4e809f3e517a1a8f0
tmucha82@gmail.com <czekolada%5>
2015-12-01 16:34:54 +0100

    Chapter 17 - collection (before sets and maps)

--- src/test/scala/com/sdc/scala/collections/CollectionsTest.scala
+++ src/test/scala/com/sdc/scala/collections/CollectionsTest.scala
@@ -3,12 +3,57 @@ package com.sdc.scala.collections
 import org.scalatest.FunSuite
 
 import scala.collection.immutable.Nil
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 class CollectionsTest extends FunSuite {
 
   test("List buffer") {
-    val buffer = new ListBuffer[Int]
+    var buffer = new ListBuffer[Int]
     assert(Nil === buffer)
+
+    buffer += 1
+    assert(ListBuffer(1) === buffer)
+    buffer += 2
+    assert(ListBuffer(1, 2) === buffer)
+
+    buffer = 3 +: buffer
+    assert(ListBuffer(3, 1, 2) === buffer)
+
+    assert(List(3, 1, 2) === buffer.result())
+    assert(3 === buffer.length)
+    assert(ListBuffer(3) === buffer.take(1))
+    assert(ListBuffer(3, 1) === buffer.take(2))
+    assert(2 === buffer(2))
   }
+
+  test("Array buffer") {
+    var buffer = new ArrayBuffer[Int]
+
+    buffer += 1
+    assert(ArrayBuffer(1) === buffer)
+    buffer += 2
+    assert(ArrayBuffer(1, 2) === buffer)
+
+    buffer = 3 +: buffer
+    assert(ListBuffer(3, 1, 2) === buffer)
+
+    assert(List(3, 1, 2) === buffer.result())
+    assert(3 === buffer.length)
+    assert(ListBuffer(3) === buffer.take(1))
+    assert(ListBuffer(3, 1) === buffer.take(2))
+    assert(2 === buffer(2))
+  }
+
+  test("Queue") {
+    //TODO
+  }
+
+  test("Stack") {
+    //TODO
+  }
+
+  test("String (via RichString)") {
+    //TODO
+  }
+
 }
