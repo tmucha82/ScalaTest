6fc4f725dd21f5508caa5260cdcfbab1d95b5444
tmucha82@gmail.com <czekolada%5>
2016-03-16 19:08:38 +0100

    Add some other stuff of XML

--- src/main/scala/com/sdc/scala/xml/CCTherm.scala
+++ src/main/scala/com/sdc/scala/xml/CCTherm.scala
@@ -4,11 +4,23 @@ abstract class CCTherm {
   val description: String
   val yearMade: Int
   val dateObtained: String
-  val bookPrice: Int      // in US cents
-  val purchasePrice: Int  // in US cents
-  val condition: Int      // 1 to 10
+  val bookPrice: Int
+  // in US cents
+  val purchasePrice: Int
+  // in US cents
+  val condition: Int // 1 to 10
 
   override def toString = description
 
   def toXML = <cctherm><description>{description}</description><yearMade>{yearMade}</yearMade><dateObtained>{dateObtained}</dateObtained><bookPrice>{bookPrice}</bookPrice><purchasePrice>{purchasePrice}</purchasePrice><condition>{condition}</condition></cctherm>
+
+  def fromXML(node: scala.xml.Node): CCTherm =
+    new CCTherm {
+      val description = (node \ "description").text
+      val yearMade = (node \ "yearMade").text.toInt
+      val dateObtained = (node \ "dateObtained").text
+      val bookPrice = (node \ "bookPrice").text.toInt
+      val purchasePrice = (node \ "purchasePrice").text.toInt
+      val condition = (node \ "condition").text.toInt
+    }
 }
--- src/test/scala/com/sdc/scala/xml/XmlTest.scala
+++ src/test/scala/com/sdc/scala/xml/XmlTest.scala
@@ -1,8 +1,11 @@
 package com.sdc.scala.xml
 
+import java.io.File
+
 import org.scalatest.FunSuite
 
-import scala.xml.NodeSeq
+import scala.io.Source
+import scala.xml.{NodeSeq, XML}
 
 class XmlTest extends FunSuite {
   test("XML literals") {
@@ -20,8 +23,7 @@ class XmlTest extends FunSuite {
   }
 
   test("ccTherm") {
-
-    val therm = new CCTherm {
+    var therm = new CCTherm {
       val description = "hot dog #5"
       val yearMade = 1952
       val dateObtained = "March 14, 2006"
@@ -31,6 +33,70 @@ class XmlTest extends FunSuite {
     }
     assert("<cctherm><description>hot dog #5</description><yearMade>1952</yearMade><dateObtained>March 14, 2006</dateObtained><bookPrice>2199</bookPrice><purchasePrice>500</purchasePrice><condition>9</condition></cctherm>" === therm.toXML.toString)
 
+    val toDeserialize = <cctherm><description>hot dog #5</description><yearMade>1952</yearMade><dateObtained>March 14, 2006</dateObtained><bookPrice>2199</bookPrice><purchasePrice>500</purchasePrice><condition>9</condition></cctherm>
+    therm = therm.fromXML(toDeserialize)
+    assert("hot dog #5" === therm.description)
+    assert(1952 === therm.yearMade)
+    assert("March 14, 2006" === therm.dateObtained)
+    assert(2199 === therm.bookPrice)
+    assert(500 === therm.purchasePrice)
+    assert(9 === therm.condition)
+
+    var source:Source = null
+    val fileName = "./therm1.xml"
+    val file = new File(fileName)
+    try {
+      XML.save(fileName, therm.toXML, "UTF-8", xmlDecl = true, null)
+      assert(file.exists())
+      source = Source.fromFile(fileName)
+      assert("<?xml version='1.0' encoding='UTF-8'?><cctherm><description>hot dog #5</description><yearMade>1952</yearMade><dateObtained>March 14, 2006</dateObtained><bookPrice>2199</bookPrice><purchasePrice>500</purchasePrice><condition>9</condition></cctherm>" === source.getLines().toList.mkString)
+
+      val loadedXml = XML.loadFile(fileName)
+      assert("<cctherm><description>hot dog #5</description><yearMade>1952</yearMade><dateObtained>March 14, 2006</dateObtained><bookPrice>2199</bookPrice><purchasePrice>500</purchasePrice><condition>9</condition></cctherm>" === loadedXml.toString)
+    } finally {
+      if(source != null) source.close()
+
+      if(file.exists()) {
+        assert(file.delete())
+      }
+    }
   }
 
+  test("escape of { char") {
+    assert("<a>{{brace yourself!}}</a>" === <a>{{{{brace yourself!}}}}</a>.toString)
+  }
+
+  test("taking XML apart") {
+    assert("Sounds good" === <a>Sounds <tag/>good</a>.text)
+    assert("input ---> output" === <a>input ---&gt; output</a>.text)
+    assert("<b><c>hello</c></b>" === (<a><b><c>hello</c></b></a> \ "b").toString())
+    assert("" === (<a><b><c>hello</c></b></a> \ "c").toString())
+    assert("<c>hello</c>" === (<a><b><c>hello</c></b></a> \\ "c").toString())
+    assert("" === (<a><b><c>hello</c></b></a> \ "a").toString())
+    assert("<a><b><c>hello</c></b></a>" === (<a><b><c>hello</c></b></a> \\ "a").toString())
+  }
+
+  test("extracting attributes") {
+    val joe = <employee name="Joe" rank="code monkey" serial="123"/>
+    assert("<employee name=\"Joe\" rank=\"code monkey\" serial=\"123\"/>" === joe.toString())
+    assert("Joe" === (joe \ "@name").toString())
+    assert("123" === (joe \ "@serial").toString())
+  }
+
+  test("xml pattern matching") {
+/*
+    object Dupa extends Enumeration {
+      val A = Value
+      val B = Value
+      val Other = Value
+    }
+
+    def proc(node: scala.xml.Node): Value =
+      node match {
+        case <a>{contents}</a> => "It's an a: " + contents
+        case <b>{contents}</b> => "It's a b: " + contents
+        case _ => "It's something else."
+      }
+*/
+  }
 }
