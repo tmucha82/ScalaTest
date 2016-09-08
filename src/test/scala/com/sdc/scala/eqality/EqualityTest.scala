c8afaa34fcc94dfb628f234061073cbfaa834331
tmucha82 <tmucha82@gmail.com>
2016-09-08 15:48:08 +0200

    Equality test final.

--- src/test/scala/com/sdc/scala/eqality/EqualityTest.scala
+++ src/test/scala/com/sdc/scala/eqality/EqualityTest.scala
@@ -128,6 +128,9 @@ class EqualityTest extends FunSuite {
       override def equals(other: Any) = other match {
         case that: ColoredPoint =>
           this.color == that.color && super.equals(that)
+        //we added new case
+        case that: Point =>
+          that.equals(this)
         case _ => false
       }
     }
@@ -138,9 +141,128 @@ class EqualityTest extends FunSuite {
     val coloredPoint = new ColoredPoint(1, 2, Color.Red)
 
     assert(point equals coloredPoint) //yep
-    assert(!(coloredPoint equals point)) //no - not symmetric
+    //assert(!(coloredPoint equals point)) //no - not symmetric, before adding case in equals
+    assert(coloredPoint equals point) //yep, after change
     assert(mutable.HashSet[Point](point) contains coloredPoint) //yep
-    assert(!(mutable.HashSet[Point](coloredPoint) contains point)) //no - not symmetric
-    //what we can do
+    //assert(!(mutable.HashSet[Point](coloredPoint) contains point)) //no - not symmetric, before adding case in equals
+    assert(mutable.HashSet[Point](coloredPoint) contains point) //yep, after change
+
+    //Now the problem is that the new relation is no longer transitive!
+
+    val redPoint = new ColoredPoint(1, 2, Color.Red)
+    val bluePoint = new ColoredPoint(1, 2, Color.Blue)
+
+    assert(redPoint equals point)
+    assert(point equals bluePoint)
+    //but....
+    assert(!(redPoint equals bluePoint))
+  }
+
+  test("Solution of Pitfall #4 part 2") {
+    object Color extends Enumeration {
+      val Red, Orange, Yellow, Green, Blue, Indigo, Violet = Value
+    }
+
+    class Point(val x: Int, val y: Int) {
+
+      def canEqual(other: Any): Boolean = other.isInstanceOf[Point]
+
+      // A better definition, but still not perfect - instead equals for Point override equals for Any
+      override def equals(other: Any): Boolean = other match {
+        case that: Point =>
+          (that canEqual this) &&
+            x == that.x &&
+            y == that.y
+        case _ => false
+      }
+
+      override def hashCode(): Int = {
+        val state = Seq(x, y)
+        state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+      }
+    }
+
+    class ColoredPoint(x: Int, y: Int, val color: Color.Value)
+      extends Point(x, y) {
+
+      override def canEqual(other: Any) = other.isInstanceOf[ColoredPoint]
+
+      override def equals(other: Any) = other match {
+        case that: ColoredPoint =>
+          (that canEqual this) && this.color == that.color && super.equals(that)
+        case _ => false
+      }
+    }
+
+    val point = new Point(1, 2)
+    val coloredPoint = new ColoredPoint(1, 2, Color.Red)
+    val differentPoint = new Point(1, 1) { override val y = 2 }
+
+    //you should compare only the same classes
+    assert(!(point equals coloredPoint)) //yep
+    assert(!(coloredPoint equals point))
+    assert(!(mutable.HashSet[Point](point) contains coloredPoint))
+    assert(!(mutable.HashSet[Point](coloredPoint) contains point))
+
+    //ss pAnon equal to p? The answer is no because the java.lang.Class objects associated with p and pAnon are different
+    //assert(!(differentPoint equals point)) //sick!!
+    assert(differentPoint equals point)
+
+    val colorCollection = List(point)
+
+    assert(colorCollection contains point)
+    assert(!(colorCollection contains coloredPoint))
+    assert(colorCollection contains differentPoint)
+    //One potential criticism of the canEqual approach is that it violates the Liskov Substitution Principle (LSP)
+  }
+
+  test("Defining equality for parametrized types") {
+    trait Tree[+T] {
+      def element: T
+      def left: Tree[T]
+      def right: Tree[T]
+    }
+
+    object EmptyTree extends Tree[Nothing] {
+      def element =
+        throw new NoSuchElementException("EmptyTree.elem")
+      def left =
+        throw new NoSuchElementException("EmptyTree.left")
+      def right =
+        throw new NoSuchElementException("EmptyTree.right")
+    }
+
+    class Branch[+T](
+                      val element: T,
+                      val left: Tree[T],
+                      val right: Tree[T]
+                    ) extends Tree[T] {
+
+      override def equals(other: Any) = other match {
+        case that: Branch[_] => (that canEqual this) &&
+          this.element == that.element &&
+          this.left == that.left &&
+          this.right == that.right
+        case _ => false
+      }
+
+      override def hashCode: Int =
+        41 * (
+          41 * (
+            41 + element.hashCode
+            ) + left.hashCode
+          ) + right.hashCode
+
+      def canEqual(other: Any) = other match {
+        case that: Branch[_] => true
+        case _ => false
+      }
+    }
+
+
+    val branch1 = new Branch[List[String]](Nil, EmptyTree, EmptyTree)
+    val branch2 = new Branch[List[Int]](Nil, EmptyTree, EmptyTree)
+    assert(branch1 == branch2)
+    assert(branch1 equals branch2)
   }
 }
