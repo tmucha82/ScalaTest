2e6fc4811279302064bcdc8562c03c3d6279746a
tmucha <tmucha@cyfrowypolsat.pl>
2016-11-22 20:41:17 +0100

    Coursera#2 more ex

---
+++ src/main/scala/org/coursera/week3/Rational.scala
@@ -0,0 +1,28 @@
+package org.coursera.week3
+
+class Rational(x: Int, y: Int) {
+  require(y != 0, "denominator must not be zero")
+
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+
+  private val g = gcd(x, y)
+
+  def numer = x / g
+
+  def denom = y / g
+
+  def this(x: Int) = this(x, 1)
+
+  def neg = new Rational(-numer, denom)
+
+  def +(that: Rational) = new Rational(numer * that.denom + that.numer * denom, denom * that.denom)
+
+  def -(that: Rational) = this + that.neg
+
+  def <(that: Rational): Boolean = this.numer * that.denom < that.numer * this.denom
+
+  def max(that: Rational): Rational = if (this < that) that else this
+
+  override def toString = numer + "/" + denom
+}
+
---
+++ src/main/scala/org/coursera/week3/Scratch.sc
@@ -0,0 +1,10 @@
+import org.coursera.week3.Rational
+
+new Rational(1, 2)
+
+def error(msg: String) = throw new Error(msg)
+
+val t = null
+val s:String = null
+
+if(true) 1 else false
