a33b9a9e20bd17d04f41b844460df7fc802d21fd
tmucha <tmucha@cyfrowypolsat.pl>
2016-12-07 21:04:50 +0100

    Chapter29 = Combining Scala and Java

---
+++ src/test/scala/com/sdc/scala/annotation/AnnotationTest.scala
@@ -0,0 +1,21 @@
+package com.sdc.scala.annotation
+
+import org.scalatest.FunSuite
+
+class AnnotationTest extends FunSuite {
+
+  @TMucha
+  def sampleMethod(): Unit = {
+  }
+
+
+  test("example") {
+
+
+    assert("sampleMethod" === (for {
+      method <- AnnotationTest.this.getClass.getMethods
+      if method.getName.startsWith("sample")
+      if method.getAnnotation(classOf[TMucha]) != null
+    } yield method.getName).head)
+  }
+}
