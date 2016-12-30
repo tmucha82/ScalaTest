0678a3ff0bff9e679d6ecaf2725edf41711a2c98
tmucha <tmucha@cyfrowypolsat.pl>
2016-12-30 20:58:36 +0100

    final solution

--- src/main/scala/com/sdc/scala/stateful/Simulation.scala
+++ src/main/scala/com/sdc/scala/stateful/Simulation.scala
@@ -4,30 +4,30 @@ abstract class Simulation {
 
   type Action = () => Unit
 
-  case class WorkItem(time: Int, action: Action)
+  case class Event(time: Int, action: Action)
 
   private var time = 0
 
   def currentTime: Int = time
 
-  private var agenda: List[WorkItem] = List()
+  private var agenda: List[Event] = List()
 
-  private def insert(ag: List[WorkItem], item: WorkItem): List[WorkItem] = {
-    if (ag.isEmpty || item.time < ag.head.time) item :: ag
-    else ag.head :: insert(ag.tail, item)
+  private def insert(events: List[Event], item: Event): List[Event] = {
+    if (events.isEmpty || item.time < events.head.time) item :: events
+    else events.head :: insert(events.tail, item)
   }
 
   def afterDelay(delay: Int)(block: => Unit) {
-    val item = WorkItem(currentTime + delay, () => block)
+    val item = Event(currentTime + delay, () => block)
     agenda = insert(agenda, item)
   }
 
   private def next() {
     (agenda: @unchecked) match {
-      case item :: rest =>
+      case event :: rest =>
         agenda = rest
-        time = item.time
-        item.action()
+        time = event.time
+        event.action()
     }
   }
 
---
+++ src/main/scala/org/coursera/scala/design/week3/BankAccounts.sc
@@ -0,0 +1,7 @@
+import org.coursera.scala.design.week3.BankAccount
+
+val account = new BankAccount
+account.deposit(50)
+account.withdraw(20)
+account.withdraw(20)
+account.withdraw(15)
