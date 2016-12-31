23d01fa78302e5d9e3676e4fa21e70841981ceb4
tmucha <tmucha@cyfrowypolsat.pl>
2016-12-31 14:45:23 +0100

    adding min to heap one more time

--- src/main/scala/org/coursera/scala/design/week3/assignment/quickcheck/QuickCheck.scala
+++ src/main/scala/org/coursera/scala/design/week3/assignment/quickcheck/QuickCheck.scala
@@ -14,9 +14,43 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
+  /**
+    * If you insert to heap element which is equals to min, then min element of
+    * nee heap should be also the same.
+    */
   property("adding min to heap one more time") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
 
+  /**
+    * If you insert to empty heap element, it must be min element
+    */
+  property("adding one element to empty heap must be min") = forAll { (a: A) =>
+    findMin(insert(a, empty)) == a
+  }
+
+  /**
+    * If you insert any two elements into an empty heap, finding the minimum of the resulting heap
+    * should get the smallest of the two elements back.
+    */
+  property("adding two elements to heap") = forAll { (a: A, b: A) =>
+    findMin(insert(a, insert(b, empty))) == a.min(b)
+  }
+
+  /**
+    * If you insert an element into an empty heap, then delete the minimum, the resulting heap should be empty.
+    */
+  //TODO
+
+  /**
+    * Given any heap, you should get a sorted sequence of elements
+    * when continually finding and deleting minima. (Hint: recursion and helper functions are your friends.)
+    */
+  //TODO
+
+  /**
+    * Finding a minimum of the melding of any two heaps should return a minimum of one or the other.
+    */
+  //TODO
 }
