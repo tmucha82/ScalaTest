4f52936b32e949895afab695b0b92760e79fb738
tmucha <tmucha@cyfrowypolsat.pl>
2016-12-31 14:26:02 +0100

    adding min to heap one more time

--- src/main/scala/org/coursera/scala/design/week3/assignment/quickcheck/QuickCheck.scala
+++ src/main/scala/org/coursera/scala/design/week3/assignment/quickcheck/QuickCheck.scala
@@ -6,11 +6,15 @@ import org.scalacheck.{Arbitrary, Gen, Properties}
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] =
+    for {
+      element <- Arbitrary.arbitrary[A]
+      heap <- Gen.oneOf(Gen.const(empty), genHeap)
+    } yield insert(element, heap)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-
-  property("gen1") = forAll { (h: H) =>
+  property("adding min to heap one more time") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
