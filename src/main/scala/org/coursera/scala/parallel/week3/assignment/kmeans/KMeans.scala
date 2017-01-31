13e8fc52ff0a9eab6b5101ce5a1b14bf1abd7a0a
tmucha <tmucha@cyfrowypolsat.pl>
2017-01-31 22:17:13 +0100

    week3 - converged method

--- src/main/scala/org/coursera/scala/parallel/week3/assignment/kmeans/KMeans.scala
+++ src/main/scala/org/coursera/scala/parallel/week3/assignment/kmeans/KMeans.scala
@@ -64,7 +64,7 @@ class KMeans {
   }
 
   def converged(eta: Double)(oldMeans: GenSeq[Point], newMeans: GenSeq[Point]): Boolean = {
-    ???
+    oldMeans.zip(newMeans).forall { case (oldMean, newMean) => oldMean.squareDistance(newMean) <= eta }
   }
 
   @tailrec
